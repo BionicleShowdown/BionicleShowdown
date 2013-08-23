e8ac3c746b3011b7dd4c4d7e7ef42dc19892b803
abcde13 <machspeeds@gmail.com>
2013-08-23 11:03:53 -0700

    Trying to pull again

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -103,7 +103,7 @@ public class Main extends SimpleApplication
     public void simpleInitApp () 
     {
         
-        stateManager.attach(new VideoRecorderAppState()); //starts recording(remove when not needed)
+        //stateManager.attach(new VideoRecorderAppState()); //starts recording(remove when not needed)
         startState = new StartMenu();
         setDisplayStatView(false);
         music = new MusicAudioNode(assetManager, "Sounds/Music/Fire and Ice.wav", true);
